2a87f64c60ea5831fc8e262d058a8fae4daf9f95
anindyou <anindyarh7@gmail.com>
2026-09-09 13:45:16 +0700

    Create main app, experience model and page, and update navigation bar looks

---
+++ main/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+from main.models import Experience
+
+
+def show_main(request):
+    context = {
+        "name": "Anindya Raihani Hassan",
+        "npm": "2506553295",
+        "study_program": "S1 Ilmu Komputer",
+        "bio": (
+            "Hello, I'm Anindya Raihani Hassan. Computer Science "
+            "student at Universitas Indonesia who combines UI/UX design and "
+            "front-end development to turn raw ideas into clean, functional, and "
+            "fully shipped projects."
+        ),
+        "short_info" : "Computer Science @ Universitas Indonesia",
+    }
+    return render(request, "index.html", context)
+
+
+def show_experience(request):
+    context = {
+        "name": "Anindya Raihani Hassan",
+        "experience_list": Experience.objects.all(),
+    }
+    return render(request, "experience.html", context)
