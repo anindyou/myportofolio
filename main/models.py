8ead02c420a974a5794ca3cdcdf1840890aa6762
anindyou <anindyarh7@gmail.com>
2026-09-11 20:09:51 +0700

    add new model (service) and migrate

--- main/models.py
+++ main/models.py
@@ -16,3 +16,12 @@ class Experience(models.Model):
     @property
     def is_ongoing(self):
         return self.ended_at is None
+
+class Service(models.Model):
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    icon = models.URLField(blank=True, null=True)
+    def __str__(self):
+        return self.title
