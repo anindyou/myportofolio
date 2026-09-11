35577d54b49f6ba92f75a24d3ea8382f241a9a2b
anindyou <anindyarh7@gmail.com>
2026-09-11 21:31:02 +0700

    change image field type and add service instances

--- main/models.py
+++ main/models.py
@@ -22,6 +22,6 @@ class Service(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=255)
     description = models.TextField()
-    icon = models.URLField(blank=True, null=True)
+    icon = models.CharField(max_length=255, blank=True, null=True)
     def __str__(self):
         return self.title
