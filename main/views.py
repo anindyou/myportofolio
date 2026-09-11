8f802007006ea7d8ada2d5a386dc325446ce4ccf
anindyou <anindyarh7@gmail.com>
2026-09-11 20:36:23 +0700

    refactor: move heading & caption to views context

--- main/views.py
+++ main/views.py
@@ -23,11 +23,16 @@ def show_experience(request):
     context = {
         "name": "Anindya Raihani Hassan",
         "experience_list": Experience.objects.all(),
+        "heading" : "Where I've Been",
+        "caption" : "A few things I've worked on and learned from along the way.",
     }
     return render(request, "experience.html", context)
 
 def show_service(request):
     context = {
-        "service_list" : Service.objects.all()
+        "name" : "Anindya Raihani Hassan",
+        "service_list" : Service.objects.all(),
+        "heading" : "What I bring to the table",
+        "caption" : "A mix of skills I've picked up, from crafting interfaces to writing the code behind them.",
     }
     return render(request, "service.html", context)
