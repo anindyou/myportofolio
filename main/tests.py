1fb8a90d51c7284659aaaef5383399f8167d2e75
anindyou <anindyarh7@gmail.com>
2026-09-12 21:45:16 +0700

    feat: fix previous test and add new service test

--- main/tests.py
+++ main/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.urls import reverse
 from django.utils import timezone
 
-from main.models import Experience
+from main.models import Experience, Service
 
 
 class MainTest(TestCase):
@@ -10,7 +10,8 @@ class MainTest(TestCase):
         self.experience = Experience.objects.create(
             title="Asisten Dosen PBP",
             description="Membantu mahasiswa memahami pengembangan web.",
-            category="part-time",
+            institution="Fasilkom UI",
+            started_at = timezone.now().date(),
         )
 
     def test_main_url_is_accessible(self):
@@ -28,7 +29,7 @@ class MainTest(TestCase):
 
     def test_experience_model(self):
         self.assertEqual(str(self.experience), "Asisten Dosen PBP")
-        self.assertEqual(self.experience.category, "part-time")
+        self.assertEqual(self.experience.institution, "Fasilkom UI")
         self.assertTrue(self.experience.is_ongoing)
 
     def test_experience_page(self):
@@ -38,21 +39,52 @@ class MainTest(TestCase):
         self.assertTemplateUsed(response, "experience.html")
         self.assertContains(response, self.experience.title)
         self.assertContains(response, self.experience.description)
-        self.assertContains(response, "Part-Time")
-        self.assertContains(response, "Sedang berlangsung")
-        self.assertContains(response, f'href="{reverse("main:show_main")}"')
+        self.assertContains(response, self.experience.institution)
+        self.assertContains(response, "On duty")
+        self.assertContains(response, f'href="{reverse("main:show_main")}#experience"')
 
     def test_empty_experience_page(self):
         Experience.objects.all().delete()
         response = self.client.get(reverse("main:show_experience"))
 
-        self.assertContains(response, "Belum ada pengalaman yang ditambahkan.")
+        self.assertContains(response, "Coming soon!")
 
     def test_completed_experience(self):
-        self.experience.ended_at = timezone.now()
+        self.experience.ended_at = timezone.now().date()
         self.experience.save()
         response = self.client.get(reverse("main:show_experience"))
 
         self.assertFalse(self.experience.is_ongoing)
-        self.assertContains(response, "Selesai")
-        self.assertNotContains(response, "Sedang berlangsung")
+        self.assertNotContains(response, "On duty")
+
+class ServiceTest(TestCase):
+    def setUp(self):
+        self.service = Service.objects.create(
+            title="UI/UX Design",
+            description="I design intuitive, user-first interfaces.",
+            icon="figma-icon.png",
+        )
+
+    def test_service_url_is_accessible(self):
+        response = self.client.get(reverse("main:show_service")) 
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "service.html")
+        self.assertContains(response, f'href="{reverse("main:show_main")}#services"') # tes balik ke main page di bagian service
+
+    def test_service_model(self):
+        self.assertEqual(str(self.service), "UI/UX Design")
+        self.assertEqual(self.service.icon, "figma-icon.png")
+
+    def test_service_page_shows_data(self):
+        response = self.client.get(reverse("main:show_service"))
+
+        self.assertContains(response, self.service.title)
+        self.assertContains(response, self.service.description)
+
+    def test_empty_service_page(self):
+        Service.objects.all().delete()
+        response = self.client.get(reverse("main:show_service"))
+
+        self.assertContains(response, "Coming soon!")
+        self.assertNotContains(response, "UI/UX Design")
